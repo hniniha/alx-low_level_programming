a67fcb4514dd2e6acb348df22f8ba5940eb31a13
hninihaa <hnini.hassan.pro@gmail.com>
2023-07-27 13:57:13 -0700

    0x12. C - Singly linked lists

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes) {
-    __asm__(
-        "mov $1, %%rax\n\t"
-        "mov $0, %%rdi\n\t"
-        "syscall"
-        :
-        : "r"(num_bytes)
-        : "rax", "rdi"
-    );
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-
-    int num_bytes = atoi(argv[1]);
-
-    if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
-
-    print_opcodes(num_bytes);
-
-    return 0;
-}
-
